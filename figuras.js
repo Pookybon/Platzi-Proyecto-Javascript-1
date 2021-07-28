3f5ae7ef1fa13d9b476ef253f496a7e58b47356e
Pookybon <yufni.castro@gmail.com>
2021-07-28 14:57:48 -0500

    Funciones encapsuladas para perimetros y areas de cada figura. El area y la altura del triangulo ahora se calcula en base al semiperimetro

--- figuras.js
+++ figuras.js
@@ -1,24 +1,36 @@
-//"Código" del cuadrado
+//Código del cuadrado
 console.group("cuadrado");
 const ladoCuadrado = 5;
+const perimetroCuadrado = (ladoCuadrado) => ladoCuadrado * 4;
+const areaCuadrado = (ladoCuadrado) => ladoCuadrado * ladoCuadrado;
 console.log(`Los lados del cuadrado miden ${ladoCuadrado}cm`);
-console.log(`El perimetro del cuadrado mide ${ladoCuadrado*4}cm`);
-console.log(`El area del cuadrado mide ${ladoCuadrado*ladoCuadrado}cm^2`);
+console.log(`El perimetro del cuadrado mide ${perimetroCuadrado(ladoCuadrado)}cm`);
+console.log(`El area del cuadrado mide ${areaCuadrado(ladoCuadrado)}cm^2`);
 console.groupEnd();
 
-//"Código" del triangulo
+//Código del triangulo
 console.group("Triangulo");
 const baseTriangulo = 5;
-const ladosTriangulo = 3;
-console.log(`La base del triagulo mide ${baseTriangulo}cm, los lados ${ladosTriangulo}cm y la altura ${Math.sqrt(ladosTriangulo**2-(baseTriangulo/2)**2)}cm`);
-console.log(`El perimetro del triangulo es ${baseTriangulo+(ladosTriangulo*2)}cm`);
-console.log(`El area del triangulo es ${(baseTriangulo*Math.sqrt(ladosTriangulo**2-(baseTriangulo/2)**2))/2}cm^2`);
+const lado1Triangulo = 4;
+const lado2Triangulo = 8;
+const perimetroTriangulo = (lado1Triangulo, lado2Triangulo, baseTriangulo) => lado1Triangulo + lado2Triangulo + baseTriangulo;
+const areaTriangulo = (lado1Triangulo, lado2Triangulo, baseTriangulo) => {
+    let semiPerimetro = (lado1Triangulo + lado2Triangulo + baseTriangulo)/2;
+    return Math.sqrt(semiPerimetro * (semiPerimetro-lado1Triangulo) * (semiPerimetro-lado2Triangulo) * (semiPerimetro-baseTriangulo));
+};
+const alturaTriangulo = (areaTriangulo, baseTriangulo) => (2 * areaTriangulo) / baseTriangulo;
+console.log(`La base del triangulo mide ${baseTriangulo}cm, los lados ${lado1Triangulo}cm, ${lado2Triangulo}cm y la altura ${alturaTriangulo(areaTriangulo(lado1Triangulo, lado2Triangulo, baseTriangulo), baseTriangulo)}cm`);
+console.log(`El perimetro del triangulo es ${perimetroTriangulo(lado1Triangulo, lado2Triangulo, baseTriangulo)}cm`);
+console.log(`El area del triangulo es ${areaTriangulo(lado1Triangulo, lado2Triangulo, baseTriangulo)}cm^2`);
 console.groupEnd();
 
-//"Código" del Circulo
+//Código del Circulo
 console.group("Circulo");
-const diametroCirculo = 5;
-console.log(`El diametro del circulo es ${diametroCirculo}cm y el radio ${diametroCirculo/2}cm`);
-console.log(`El perimetro del circulo es ${diametroCirculo*Math.PI}cm`);
-console.log(`El area del circulo es ${Math.PI*(diametroCirculo/2)**2}cm^2`);
+const radioCirculo = 6;
+const diametroCirculo = (radioCirculo) => radioCirculo*2;
+const perimetroCirculo = (radioCirculo) => radioCirculo*2*Math.PI;
+const areaCirculo = (radioCirculo) => Math.PI*(radioCirculo)**2;
+console.log(`El radio del circulo es ${radioCirculo}cm y el diametro ${diametroCirculo(radioCirculo)}cm`);
+console.log(`El perimetro del circulo es ${perimetroCirculo(radioCirculo)}cm`);
+console.log(`El area del circulo es ${areaCirculo(radioCirculo)}cm^2`);
 console.groupEnd();
